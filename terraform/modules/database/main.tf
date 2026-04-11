258cd819aa49f814c824094ee5b96a8115526747
EidAbokor1 <abokoreid1@gmail.com>
2026-04-11 18:51:12 +0100

    fix checkov findings enable deletion protection, auto upgrades, performance insights

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -24,8 +24,12 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [var.rds_security_group_id]
   
-  publicly_accessible = false
-  skip_final_snapshot = true
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  deletion_protection    = true
+  auto_minor_version_upgrade = true
+  
+  performance_insights_enabled = true
   
   backup_retention_period = 1
   backup_window           = "03:00-04:00"
